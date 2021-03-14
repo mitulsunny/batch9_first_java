dc247bb7cfc30d7c91186dd2a24bc2c3f69ee90b
sayeedin <sayeedins@yahoo.com>
2021-03-14 12:21:12 -0400

    This is sayeedin

--- src/main/java/com/osa/base/Browser.java
+++ src/main/java/com/osa/base/Browser.java
@@ -1,7 +1,23 @@
 package com.osa.base;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+@Test
 public class Browser {
-public static void main(String[] args) {
-	System.out.println("Testing");
-}
+
+	 WebDriver driver;
+	public  void browserOpen() {
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("https://www.osaconsultingtech.com");
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+		
+	}
 }
