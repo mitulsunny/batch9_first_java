0bd5cc1968456677430c367238d60b3cbb38c48f
amirhossain478 <80213632+amirhossain478@users.noreply.github.com>
2021-03-15 14:19:14 -0400

    This is the commit for HomePage

--- src/test/java/com/osa/pages/HomePage.java
+++ src/test/java/com/osa/pages/HomePage.java
@@ -1,5 +1,16 @@
 package com.osa.pages;
 
 public class HomePage {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("I wish to learn how to use gitHub");
+		
+	}
+	
+
+
+
+
 
 }
