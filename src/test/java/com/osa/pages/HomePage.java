b2361093494e445236f9fa34ab1c5d50493d327c
Md Asif Khan <khanasif31720@gmail.com>
2021-03-15 22:56:39 -0400

    Everytime fail

--- src/test/java/com/osa/pages/HomePage.java
+++ src/test/java/com/osa/pages/HomePage.java
@@ -4,12 +4,9 @@ public class HomePage {
 	
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
+
 		System.out.println("Hi! friends thanks for your help");
-=======
-		System.out.println("First time i developed the code with GitHub. It's very easy to work together.");
->>>>>>> 6b09e0ac1b4aa785cd1d03bbf87f40bd522ed6cb
-		
+		System.out.println("First time i developed the code with GitHub. It's very easy to work together.");		
 	}
 	
 
